c25c3e90fada6fec1e1a7a638c133c8205e655bc
SimoneChiodo <simochi.005@gmail.com>
2025-11-13 16:33:36 +0100

    Add employee_detail page

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import Http404
+from django.shortcuts import get_object_or_404, render
+from employees.models import Employee
 
-# Create your views here.
+def employee_detail(request, pk):
+  employee = get_object_or_404(Employee, pk=pk)
+  context = {
+    'first_name': employee.first_name,
+    'last_name': employee.last_name,
+    'photo': employee.photo,
+    'designation': employee.designation,
+    'email_address': employee.email_address,
+    'phone_number': employee.phone_number,
+    'created_at': employee.created_at,
+    'updated_at': employee.updated_at,
+  }
+  return render(request, 'employee_detail.html', context)
